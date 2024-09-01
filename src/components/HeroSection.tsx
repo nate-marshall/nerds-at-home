ef33df34b836f8a84f6e27b954e30b8540b536dc
Nate Marshall <46655553+nate-marshall@users.noreply.github.com>
2024-09-01 18:30:52 -0500

    cleanup for builds

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,6 @@ const HeroSection: React.FC = () => {
       <div className="container">
         <h1 className="fade-in">N E R D S @ H O M E</h1>
         <p className="fade-in">Create a stylish app landing page and highlight all the great things about your product!</p>
-        <a href="#" className="btn btn-primary fade-in">Download App</a>
       </div>
     </section>
   );
